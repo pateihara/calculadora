0546a6cd65110c572a3395bd7281308f6e985c6e
Patricia Eihara <pateihara@gmail.com>
2024-02-26 08:07:40 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+//1
+console.log("Hello world!");
+
+//2
+function myFunction() {
+  alert("Hello World");
+}
+
+//3
+document.getElementById("hello").innerHTML = "Hello World";
+
+//4
+let text = document.getElementById("MeuTexto").innerText;
+document.getElementById("Substituir").innerHTML = text;
+
+//6
+document.getElementById("Lista").innerHTML = "Hello World";
+
+//7
+alert(`Hello World`);
+
+//8
+document.write("Hello World");
+
+//9
+function helloWorld() {
+  return "Hello World 9";
+}
+
+console.log(helloWorld());
+
+//10
+const OlaMundo = ["Hello", "World", "!"];
+document.getElementById("ListaMundo").innerHTML = OlaMundo;
