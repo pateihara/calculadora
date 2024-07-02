998efd8949959985472bbf33b0116287a88b352e
Patricia Eihara <pateihara@gmail.com>
2024-07-02 09:13:53 -0300

    finalização

---
+++ README.md
@@ -0,0 +1,21 @@
+Criar uma calculadora estilizada com CSS e ela deve fazer as 4 operações matemáticas:
+soma,
+subtração,
+multiplicação ou
+divisão.
+
+● Deve ser exibido botões numéricos de 0 a 9.
+● Deve ser exibido botões das operações
+● Deve exibir o botão para adicionar um vírgula(para operações com decimais)
+● Deve exibir o botão igual (para executar a operação digitada)
+● O campo de texto deve responder aos cliques nos botõese ao digitar no teclado numérico
+● A aplicação deve ser responsiva, funcionando no desktop e mobile
+
+Avaliação
+Organizar bem os commits e mensagens
+Estilizar responsivamente a calculadora
+Realizar as quatro operações matemáticas
+Responder aos cliques nos botões da tela
+Responder ao digitar os valores pelo teclado numérico
+Aplicar lógicas booleanas corretamente
+Criar funções para organizar o código
--- src/index.js
+++ src/index.js
@@ -1,34 +1,133 @@
-//1
-console.log("Hello world!");
+// function onSubmit(event) {
+//   event.preventDefault();
+//   console.log("tá funcionand");
+// }
 
-//2
-function myFunction() {
-  alert("Hello World");
+// const form = document.querySelector("form");
+// form.addEventListener("load", onSubmit);
+
+// const buttonSubmit = document.getElementById("buttonSubmit");
+// buttonSubmit.addEventListener("click", () => {
+//   console.log("funcionando");
+// });
+
+// Quando o usuário digitar o valor deverá aparecer na tela RESULTADO
+// Terá que armazenar quando clicar em algum OPERADOR
+// Quando o usuário digitar o valor deverá aparecer na tela RESULTADO
+// Caso ele apertar um Operador, já deverá fazer a operação do anterior e armazenar esse valor
+// Caso ele apertar o botão IGUAL, só somará os valores
+
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+function updateResult(originClear = false) {
+  result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
 }
 
-//3
-document.getElementById("hello").innerHTML = "Hello World";
+function addDigit(digit) {
+  if (digit === "," && (currentNumber.includes(",") || !currentNumber)) return;
 
-//4
-let text = document.getElementById("MeuTexto").innerText;
-document.getElementById("Substituir").innerHTML = text;
+  if (restart) {
+    currentNumber = digit;
+    restart = false;
+  } else {
+    currentNumber += digit;
+  }
 
-//6
-document.getElementById("Lista").innerHTML = "Hello World";
-
-//7
-alert(`Hello World`);
-
-//8
-document.write("Hello World");
-
-//9
-function helloWorld() {
-  return "Hello World 9";
+  updateResult();
 }
 
-console.log(helloWorld());
+function setOperator(newOperator) {
+  if (currentNumber) {
+    calculate();
 
-//10
-const OlaMundo = ["Hello", "World", "!"];
-document.getElementById("ListaMundo").innerHTML = OlaMundo;
+    firstOperand = parseFloat(currentNumber.replace(",", "."));
+    currentNumber = "";
+  }
+
+  operator = newOperator;
+}
+
+function calculate() {
+  if (operator === null || firstOperand === null) return;
+  let secondOperand = parseFloat(currentNumber.replace(",", "."));
+  let resultValue;
+
+  switch (operator) {
+    case "+":
+      resultValue = firstOperand + secondOperand;
+      break;
+    case "-":
+      resultValue = firstOperand - secondOperand;
+      break;
+    case "×":
+      resultValue = firstOperand * secondOperand;
+      break;
+    case "÷":
+      resultValue = firstOperand / secondOperand;
+      break;
+    default:
+      return;
+  }
+
+  if (resultValue.toString().split(".")[1]?.length > 5) {
+    currentNumber = parseFloat(resultValue.toFixed(5)).toString();
+  } else {
+    currentNumber = resultValue.toString();
+  }
+
+  operator = null;
+  firstOperand = null;
+  restart = true;
+  percentageValue = null;
+  updateResult();
+}
+
+function clearCalculator() {
+  currentNumber = "";
+  firstOperand = null;
+  operator = null;
+  updateResult(true);
+}
+
+function setPercentage() {
+  let result = parseFloat(currentNumber) / 100;
+
+  if (["+", "-"].includes(operator)) {
+    result = result * (firstOperand || 1);
+  }
+
+  if (result.toString().split(".")[1]?.length > 5) {
+    result = result.toFixed(5).toString();
+  }
+
+  currentNumber = result.toString();
+  updateResult();
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonText = button.innerText;
+    if (/^[0-9,]+$/.test(buttonText)) {
+      addDigit(buttonText);
+    } else if (["+", "-", "×", "÷"].includes(buttonText)) {
+      setOperator(buttonText);
+    } else if (buttonText === "=") {
+      calculate();
+    } else if (buttonText === "C") {
+      clearCalculator();
+    } else if (buttonText === "±") {
+      currentNumber = (
+        parseFloat(currentNumber || firstOperand) * -1
+      ).toString();
+      updateResult();
+    } else if (buttonText === "%") {
+      setPercentage();
+    }
+  });
+});
